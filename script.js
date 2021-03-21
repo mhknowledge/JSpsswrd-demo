2e95719a883c0909692feb86e29f8fabb80ee980
mhknowledge <79174643+mhknowledge@users.noreply.github.com>
2021-03-21 14:36:20 -0400

    update to hw assignment

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", function () {
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+});
 //Password Complexity Variables
 var checkLength;
 var checkLowerCase;
@@ -13,92 +19,98 @@ var charSpecial = ["!","@","#","$","%","^","&","*","(",")","-","+","{","}","[","
 
 var userOptions;
 
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-
-var generatePassword = function() {
-//User options/choices for password prompts
-checkLength = window.prompt("How many character will your password contain?  Minimun is 8, Maximum is 128...");
-
-//If user does not input a length
-  if(!checkLength){
-      window.alert("A password length is required!");
+function generatePassword() {
+  //User options/choices for password prompts
+  checkLength = window.prompt("How many character will your password contain?  Minimun is 8, Maximum is 128...");
   
-
-  }  //If submitted length fails to meet requirement   
-  while (checkLength < 8 || checkLength > 128) {
-      checkLength = window.prompt("Length does not meet requirements.  Please try again! " + "Min: 8, Max: 128");
-  }    
-  if (checkLength == 8 || checkLength <= 128) {
-      checkLowerCase = window.confirm("Will your password have Lowercase letters?");
-      checkUpperCase = window.confirm("Will your password have Capital letters?");
-      checkNumeric = window.confirm("Will your password include Numbers?");
-      checkSpecial = window.confirm("Will your password have Special Characters?");
-    }
-
-//Confirm conditions for password criteria
+  //If user does not input a length
+    if(!checkLength){
+        window.alert("A password length is required!");
+    
+  
+    }  //If submitted length fails to meet requirement   
+    while (checkLength < 8 || checkLength > 128) {
+        checkLength = window.prompt("Length does not meet requirements.  Please try again! " + "Min: 8, Max: 128");
+    }    
+    if (checkLength == 8 || checkLength <= 128) {
+        checkLowerCase = window.confirm("Will your password have Lowercase letters?");
+        checkUpperCase = window.confirm("Will your password have Capital letters?");
+        checkNumeric = window.confirm("Will your password include Numbers?");
+        checkSpecial = window.confirm("Will your password have Special Characters?");
+      };
+      //Confirm conditions for password criteria
 if (!checkLowerCase && !checkUpperCase && !checkNumeric && !checkSpecial) {
-    userOptions = window.alert("You must set a criteria for your password to satisfy complexity requirements!");
-    generatePassword();
-     
+  userOptions = window.alert("You must set a criteria for your password to satisfy complexity requirements!");
+  //generatePassword();
+   
 }
-if (checkLowerCase && checkUpperCase && checkNumeric && checkSpecial) {
+else if (checkLowerCase && checkUpperCase && checkNumeric && checkSpecial) {
   userOptions = charLow.concat(charUp, num, charSpecial);
 }
 //coding for passwords with 3 criterias
-if (checkLowerCase && checkUpperCase && checkNumeric){
+else if (checkLowerCase && checkUpperCase && checkNumeric){
   userOptions = charLow.concat(charUp, num);
 
-} if (checkLowerCase && checkUpperCase && checkSpecial){
+} else if (checkLowerCase && checkUpperCase && checkSpecial){
   userOptions = charLow.concat(charUp, charSpecial);
 
-} if (checkLowerCase && checkNumeric && checkSpecial){
+} else if (checkLowerCase && checkNumeric && checkSpecial){
   userOptions = charLow.concat(num, charSpecial);
 
-} if (checkUpperCase && checkNumeric && checkSpecial){
+} else if (checkUpperCase && checkNumeric && checkSpecial){
   userOptions = charUp.concat(num, charSpecial);
 }
 
 //coding for password with 2 criterias
-if (checkLowerCase && checkUpperCase){
+else if (checkLowerCase && checkUpperCase){
   userOptions = charLow.concat(charUp);
-} if (checkLowerCase && checkNumeric){
+} else if (checkLowerCase && checkNumeric){
   userOptions = charLow.concat(num);
-} if (checkLowerCase && checkSpecial){
+} else if (checkLowerCase && checkSpecial){
   userOptions = charLow.concat(charSpecial);
 }
-if (checkUpperCase && checkNumeric){
+else if (checkUpperCase && checkNumeric){
   userOptions = charUp.concat(num);
-} if (checkUpperCase && checkSpecial){
+} else if (checkUpperCase && checkSpecial){
   userOptions = charUp.concat(charSpecial);
 }
-if (checkNumeric && checkSpecial){
+else if (checkNumeric && checkSpecial){
   userOptions = num.concat(charSpecial);
 }
 //coding for passwords with 1 criteria
-if (checkLowerCase){
+else if (checkLowerCase){
   userOptions = charLow;
-}if (checkUpperCase){
+} else if (checkUpperCase){
   userOptions = charUp;
-} if (checkNumeric){
+} else if (checkNumeric){
   userOptions = num;
-} if (checkSpecial){
+} else if (checkSpecial){
   userOptions = charSpecial;
+};
+// password variable is an array placeholder to store user selected length
+var password = [];
+
+//random selection of password's variables
+for (var i = 0; i < checkLength; i++) {
+    var pickChoices = userOptions[Math.floor(Math.random() * userOptions.length)];
+    password.push(pickChoices);
 }
+//check to see if random selection is working properly, log result/candidate to console
+console.log(password);
 
-return userOptions;
+//convert password array into string for text output
+var ps = password.join("");
+//test the converted array to string, log result to console
+console.log(ps);
+writePassword(ps);
 
- }
 
-// Write password to the #password input
+return ps;
+    }
+//Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-  
+    
+  var password = document.querySelector("#password");
 
 }
 
